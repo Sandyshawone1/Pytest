6cc12f98bfe65a21c34e53c1cd9b604b424a5292
sandeep <sandydeep56@gmail.com>
2026-01-17 18:03:21 +0530

    Pytest

--- Day 2/Concatenation.py
+++ Day 2/Concatenation.py
@@ -1,10 +1,44 @@
-print(10+10)
-print(10.8+10.7)
 
-print("Welcome" + "Python")
+# num1=input("Enter first number:")
+# num2=input("Enter second number:")
+#
+# print(type(num1))
+# print(type(num2))
+#
+# print(num1+num2)  #100200
 
-print(True + 5)  # True = 1    Falase = 0
-print(True - 5)
 
-print(True * 5)
-print(True / 5)
+#Approach1
+
+# num1=int(input("Enter first number:"))
+# num2=int(input("Enter second number:"))
+#
+# print(type(num1))
+# print(type(num2))
+#
+# print(num1+num2)  #300
+
+
+
+#Appraoch2
+# num1=input("Enter first number:")
+# num2=input("Enter second number:")
+#
+# print(int(num1)+int(num2))
+
+
+
+#Example for decimal - float
+
+num1=input("Enter first decimal number:")
+num2=input("Enter second decimal number:")
+
+print(num1+num2)  #10.520.0
+
+print(float(num1)+float(num2))  #30.5
+
+
+
+
+
+
