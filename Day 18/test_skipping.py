10e9e2019ab89a9945972c624bac8e180541c62d
sandeep <sandydeep56@gmail.com>
2026-02-02 20:37:25 +0530

    Pytest skipping test

---
+++ Day 18/test_skipping.py
@@ -0,0 +1,22 @@
+import pytest
+
+def test_loginbyemail():
+    print("test_loginbyemail")
+    assert 1==1
+
+
+def test_loginbyfacebook():
+    print("test_loginbyfacebook")
+    assert 1 == 1
+@pytest.mark.skip
+def test_loginbytwitter():
+    print("test_loginbytwitter")
+    assert 1 == 1
+
+def test_loginbyinstagram():
+    print("test_loginbyinstagram")
+    assert 1 == 1
+
+def test_loginbyinstagrm_facebook():
+    print("test_loginbyinstagrm_facebook")
+    assert 1 == 1
